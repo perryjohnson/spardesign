06e785997ab34206138d250cead28f40e7283c5f
Perry Johnson <perryjohnson@ucla.edu>
2012-01-20 14:17:50 -0800

    cleaned up, forgot to delete files from the main directory

--- makeVABSobjects.py
+++ makeVABSobjects.py
@@ -12,7 +12,7 @@ plot_grid_flag = True
 plot_layer_flag = False
 plot_material_flag = False
 startrange = 23   # run the 23rd spar station
-maxAR = 1000
+maxAR = 5  # according to PreVABS, the cell aspect ratio is usually set from 3.0-8.0 ... maybe 1.2 is too small (high mem usage!)
 vabs_filename = 'test_input_file5.dat'
 
 
