c1684fc8ada6ed7dd0fcdecea2bff96b311b20e4
Perry Johnson <perryjohnson@ucla.edu>
2012-05-02 13:03:45 -0700

    added biplane spar 14, rj=0.629, g=0.75; procedures for making new biplane spar configurations

--- DYMORE/makeSingleMKblock.py
+++ DYMORE/makeSingleMKblock.py
@@ -1,17 +1,15 @@
 import DYMOREutilities as du
-
-dymoreMKfile = du.makeFile('spar_station_16__k2_0019_MK.dat')
-
-vabsMK = '../VABS/cs_database/biplane_curved/spar_station_16__k2_0019.dat.K'
-
 import truegrid.read_layup as rl
 
-layup_data = rl.readLayupFile('../truegrid/biplane_cross-sections_layup.txt')
-
+# parameters #####################################################################
 spar_station = 16
+dymoreMKfile = du.makeFile('spar_station_16__k2_0016_MK.dat')
+vabsMK = '../VABS/cs_database/biplane_curved/spar_station_16__k2_0016.dat.K'
+##################################################################################
 
+
+# write the mass and stiffness matrices as a DYMORE-formatted file
+layup_data = rl.readLayupFile('../truegrid/biplane_cross-sections_layup.txt')
 stationData = rl.extractStationData(layup_data,spar_station)
-
 du.writeMKmatrices(dymoreMKfile, vabsMK, stationData, CoordType='ETA_COORDINATE', debug_flag=True)
-
 dymoreMKfile.close()
